cec32cbea1bf1002dd754d09400223dc6bac1b38
akomykel <akomykel@gmail.com>
2025-02-25 17:42:09 +0800

    Deploy sample AR

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ AFRAME.registerComponent('markerhandler', {
             // redirect to custom URL
             //window.location = 'https://github.com/AR-js-org/AR.js';
             // alert('hello ar');
-            var et = document.querySelector('#mario');
+            var et = document.querySelector('#testentity');
             et.innerHTML="<h1>Hello AR</h1>";
         });
     }
