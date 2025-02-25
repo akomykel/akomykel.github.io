6e80baeaa30934806ad72b183096c1019d2702d5
akomykel <akomykel@gmail.com>
2025-02-25 14:34:33 +0800

    Deploy sample AR

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
-var m = document.querySelector("a-marker")
+var m = document.querySelector("a-marker");
+
 m.addEventListener("markerFound", (e)=>{
-console.log("found")
-document.getElementById("HeadText").style.visibility = "visible";
-    })
+    console.log("found");
+    alert('marker found');
+    document.getElementById("HeadText").style.visibility = "visible";
+});
 
 m.addEventListener("markerLost", (e)=>{
-console.log("lost");
-document.getElementById("HeadText").style.visibility = "hidden";
-})
+    console.log("lost");
+    alert('marker lost');
+    document.getElementById("HeadText").style.visibility = "hidden";
+});
