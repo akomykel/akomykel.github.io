48a03b18d16163df48ff29cab105abf748721bbd
akomykel <akomykel@gmail.com>
2025-02-25 15:29:40 +0800

    Deploy sample AR

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
-var m = document.querySelector("a-marker");
-
-m.addEventListener("markerFound", (e)=>{
-    console.log("found");
-    alert('marker found');
-    document.getElementById("HeadText").style.visibility = "visible";
-});
-
-m.addEventListener("markerLost", (e)=>{
-    console.log("lost");
-    alert('marker lost');
-    document.getElementById("HeadText").style.visibility = "hidden";
+AFRAME.registerComponent('markerhandler', {
+    init: function () {
+        this.el.sceneEl.addEventListener('markerFound', () => {
+            // redirect to custom URL
+            window.location = 'https://github.com/AR-js-org/AR.js';
+        });
+    }
 });
