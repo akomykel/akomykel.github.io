e127f00a5d64ec548a8275cc83e312d1c9e28818
akomykel <akomykel@gmail.com>
2025-02-25 15:37:16 +0800

    Deploy sample AR

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ AFRAME.registerComponent('markerhandler', {
     init: function () {
         this.el.sceneEl.addEventListener('markerFound', () => {
             // redirect to custom URL
-            window.location = 'https://github.com/AR-js-org/AR.js';
+            //window.location = 'https://github.com/AR-js-org/AR.js';
+            document.getElementById('testing').style.display='blocked';
         });
     }
 });
