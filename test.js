5b8758c50646d2806e7816d17508dabc98d18839
akomykel <akomykel@gmail.com>
2025-02-26 08:59:23 +0800

    Adding button to sample AR

--- test.js
+++ test.js
@@ -3,11 +3,9 @@ AFRAME.registerComponent('markerhandler', {
         this.el.sceneEl.addEventListener('markerFound', () => {
             // redirect to custom URL
             //window.location = 'https://github.com/AR-js-org/AR.js';
-            alert('hello ar');
-            var et = document.getElementById('refresh-button');
-            et.addEventListener('click', function() {
-                alert('button clicked!');
-            })
+            alert('test popup');
+
+            
         });
     }
 });
